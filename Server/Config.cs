d48ccbfa98acf034ba88afc12c424647583ef9a1
辉 冯 <695979933@qq.com>
2019-09-27 08:52:58 +0800

    添加项目文件。

---
+++ Server/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    /// <summary>
+    ///     Identity配置
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        ///     定义要保护的资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources() {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+        /// <summary>
+        ///     定义授权客户端
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients() {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, //设置模式，客户端模式
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddIdentityServer()//IdentityServer4服务
+               .AddDeveloperSigningCredential()
+               .AddInMemoryApiResources(Config.GetApiResources()) //配置资源
+               .AddInMemoryClients(Config.GetClients());//把配置文件的Client配置资源放到内存
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseIdentityServer();
+            app.UseMvc();
+        }
+    }
+}
