5fc786cbb3472c213c6dadff88ad0bc2eb50aa63
辉 冯 <695979933@qq.com>
2019-09-28 10:26:04 +0800

    add ResourceOwnerPassword

---
+++ Quickstarts/ResourceOwnerPassword/Server/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace Server
+{
+    public class Config
+    {
+        /// <summary>
+        ///     定义要保护的资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources() {
+            return new List<ApiResource>
+            {
+               new ApiResource("api1","MyApi")
+            };
+        }
+        /// <summary>
+        ///     定义授权客户端
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients() {
+            return new List<Client>
+            {
+                new Client(){ 
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    AccessTokenLifetime=60,
+                    ClientSecrets=
+                    {
+                      new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "api1",IdentityServerConstants.StandardScopes.OfflineAccess //如果要获取refresh_tokens ,必须在scopes中加上OfflineAccess
+                    },
+                    AllowOfflineAccess=true// 主要刷新refresh_token,
+          
+                }
+            };
+        }
+    }
+}
---
+++ Quickstarts/ResourceOwnerPassword/Server/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,29 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            if (context.UserName == "test" && context.Password == "test")
+            {
+                context.Result = new GrantValidationResult(
+                 subject: context.UserName,
+                 authenticationMethod: OidcConstants.AuthenticationMethods.Password);
+            }
+            else
+            {
+                //验证失败
+                context.Result = new GrantValidationResult(
+                    TokenRequestErrors.InvalidGrant,
+                    "invalid custom credential"
+                    );
+            }
+            return Task.FromResult(0);
+        }
+    }
+}
